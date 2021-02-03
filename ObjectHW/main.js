c83adf7dd463e82c3b31f97076bd9d52aab5261f
Darius-Cooper <77126445+Darius-Cooper@users.noreply.github.com>
2021-02-02 18:27:43 -0800

    Update main.js

--- ObjectHW/main.js
+++ ObjectHW/main.js
@@ -4,11 +4,11 @@ var movies = [];
 // function add to movie object to array
 function add(){
         // fetching movie title from title id
-        var title = document.getElementById('title').value;
+        let title = document.getElementById('title').value;
         // fething rating
-        var rating = document.getElementById('rating').value;
+        let rating = document.getElementById('rating').value;
         // creating object of constructor function Movie
-        var movie = new Movie(title, rating);
+        let movie = new Movie(title, rating);
         // calling validate method of movie to add movie object to array
         if(movie.validate()){
                 // push to array
@@ -25,9 +25,9 @@ function add(){
 
 // function display to display list
 function display(){
-        var htmlText = "Movies: <ul>";
+        let htmlText = "Movies: <ul>";
         // looping through each movie in list
-        for(var i=0;i<movies.length;i++){
+        for(let i=0;i<movies.length;i++){
                 htmlText += "<li>"+movies[i].toString()+"</li>";
         }
         // ending ul
