6202ca8d300104da1d94ea5116f265f20e04c18a
Plat-X <davydworzak@outlook.com>
2021-02-06 04:05:06 -0800

    Update file

--- ObjectHW/main.js
+++ ObjectHW/main.js
@@ -1,19 +1,4 @@
-// function display to display list
-function display(){
-        let htmlText = "Movies: <ul>";
-        // looping through each movie in list
-        for(let i=0;i<movies.length;i++){
-                htmlText += "<li>"+movies[i].toString()+"</li>";
-        }
-        // ending ul
-        htmlText += "</ul>";
-        // adding to display div in html
-        document.getElementById('display').innerHTML = htmlText;
-}
-
-
 document.getElementById('displayArea').style.display = 'none';
-
 document.getElementById('addMovieButton').addEventListener('click', addMovie);
 document.getElementById('showMoviesButton').addEventListener('click', showMovies);
 
@@ -21,8 +6,8 @@ document.getElementById('showMoviesButton').addEventListener('click', showMovies
 let movieArray = [];
 
 function addMovie() {
-    // creating object of constructor function MovieRating
-    let movie = new MovieRating(
+    // creating object of constructor function Movie
+    let movie = new Movie(
         // fetching movie title from title id
         document.getElementById('titleInput').value,
         // fetching rating
@@ -42,7 +27,7 @@ function addMovie() {
         document.getElementById("titleInput").select();
 }
 
-// function display to display list
+// function to display list
 function showMovies(){
         document.getElementById('displayArea').style.display = 'block';
         document.getElementById('myUl').innerHTML = "";
@@ -51,6 +36,6 @@ function showMovies(){
                 let myLi = document.createElement('li');
                 document.getElementById('myUl').appendChild(myLi);
                 myLi.innerHTML = movieArray[i].toString();
-        };
+        }
         document.getElementById("titleInput").select();
 }
--- ObjectHW/movie.js
+++ ObjectHW/movie.js
@@ -1,22 +1,22 @@
-// constructor function MovieRating
-let MovieRating = function(pTitle, pRating) {
+// constructor function Movie
+let Movie = function(pTitle, pRating) {
         // property title
-        this.title = pTitle;
+        this.title = pTitle,
         // property rating
-        this.rating = parseInt(pRating);
-    }
+        this.rating = parseInt(pRating)
+};
 
 // validate method to validate title and rating
-MovieRating.prototype.isValid = function(){
+Movie.prototype.isValid = function(){
         // if title is valid and rating is between 1 and 5 return true
         if (this.title === "" || this.rating === "" || isNaN(this.rating) || this.rating >5 || this.rating <1) {
                 return false;
         } else {
                 return true;
         }
-}
+};
 
 // toString method
-MovieRating.prototype.toString = function(){
+Movie.prototype.toString = function(){
         return 'Title: ' + this.title + ' Rating: ' + this.rating;
-}
+};
